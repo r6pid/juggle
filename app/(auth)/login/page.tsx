7ff76df17406835c27b546f1cb61460febcb8c55
Sean Hang <notsean2020@gmail.com>
2025-01-10 21:10:11 -0800

    Start of login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,15 +1,20 @@
-import { signIn } from "@/auth";
+"use client"
+
+import { signIn } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 
 export default function LoginPage() {
 	return (
-		<form
-			action={async () => {
-				"use server";
-				await signIn("google");
-			}}
-		>
-			<Button type="submit">Log in with Google</Button>
-		</form>
+		<div className="flex justify-center items-start min-h-screen">
+			<div className="pt-4">
+				<h1>
+					Welcome Back
+				</h1>
+				<h2>
+					Provide your details to login to juggle
+				</h2>
+				<Button onClick={() => signIn("google")}>Login with Google</Button>
+			</div>
+		</div>
 	);
 }

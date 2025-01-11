7327908d945d2e1b0543ac9e54387fae9387f7d5
ishaankumar09 <ishaankumar2009@gmail.com>
2025-01-11 04:28:12 -0800

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,14 +7,14 @@ import { LogoutButton } from "./LogoutButton";
 export default async function Navbar() {
 	const session = await auth();
 	return (
-		<nav className="fixed w-full bg-background top-0 z-50 grid h-14 grid-cols-[1fr_auto] items-center gap-4 border-b-border border-b px-4">
+		<nav className="fixed w-full bg-zinc-50 top-0 z-50 grid h-14 grid-cols-[1fr_auto] items-center gap-4 border-b-border border-b px-4">
 			<div>
 				<Link
 					className="flex w-fit flex-row items-center justify-center gap-3"
 					href="/"
 				>
 					<Image src={"/logo.png"} alt="Logo" width={40} height={40} />
-					<p className="text-sm">Juggle</p>
+					<p className="text-lg">Juggle</p>
 				</Link>
 			</div>
 
@@ -30,7 +30,12 @@ export default async function Navbar() {
 							</Button>
 						</div>
 					) : (
-						<LogoutButton />
+						<>
+							<Button className="w-full" asChild>
+								<Link href="/dashboard">Dashboard</Link>
+							</Button>
+							<LogoutButton />
+						</>
 					)}
 				</div>
 			</div>
